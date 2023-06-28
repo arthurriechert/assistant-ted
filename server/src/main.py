cf44934ffe5afcfd8b76cf9f1e3023097e201203
Arthur Riechert <riechertarthur@gmail.com>
2023-06-28 04:04:43 -0400

    Added endpoint for chat websocket

--- server/src/main.py
+++ server/src/main.py
@@ -5,15 +5,17 @@ from chat import ChatManager
 app = FastAPI()
 chat = ChatManager()
 
-@app.onevent("shutdown")
+@app.on_event("shutdown")
 async def chat_shutdown():
     del chat
 
-@app.websocket("/chat")
+@app.websocket("api/chat")
 async def get_response(websocket: WebSocket):
     await websocket.accept()
     while True:
-        for message in chat.talk("It's late at night")
-            print(message)
-            await websocket.send_text(message)
+        user_message = await websocket.received_text()
+        print(f"Received message from user: {user_message}")
+        for assistant_message in chat.talk(user_message):
+            print(f"Received message from AI: {assistant_message}.")
+            await websocket.send_text(assistant_message)
 
